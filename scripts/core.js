bc7ef1992be093009efb9d960fb6ad1d7e48911b
MatvienkoDO <chastor97@gmail.com>
2019-07-07 15:21:42 +0400

    improvement of render via passing context to it

--- scripts/core.js
+++ scripts/core.js
@@ -1,6 +1,15 @@
 (function() {
+	const canvas = document.getElementById('canvas')
+	if(!canvas) {
+		throw 'canvas was not found'
+	}
+	const context = canvas.getContext('2d')
+	if(!context) {
+		throw 'context was not found'
+	}
+
 	function requestAnimationFrameCallback(timestamp) {
-		render(timestamp)
+		render(timestamp, context)
 		window.requestAnimationFrame(requestAnimationFrameCallback)
 	}
 
