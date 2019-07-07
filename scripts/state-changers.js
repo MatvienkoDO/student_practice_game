0676003aa3c7a4eb3671160cc23e3323e456902b
MatvienkoDO <chastor97@gmail.com>
2019-07-07 15:32:57 +0400

    fixes in startGame state changer

--- scripts/state-changers.js
+++ scripts/state-changers.js
@@ -2,7 +2,11 @@ const stateChangers = (function(){
 	return {
 		startGame: (playerName) => {
 			state.playerName = playerName
+			state.score = 0
+			state.health = 100
+			state.mana = 100
 			state.startTime = new Date()
+			state.playerPos = { x: 0, y: 0 } // todo: temporary
 		}
 	}
 })()
