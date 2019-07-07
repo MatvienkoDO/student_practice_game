099ee1cee45bfe8816d475fc9923b77ff702bd72
MatvienkoDO <chastor97@gmail.com>
2019-07-07 20:03:19 +0400

    config for health bar and mana bar

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,4 +1,28 @@
 const constants = (function(){
 	return {
+		healthBarConfig: {
+			borderColor: 'black',
+			borderWidth: 2,
+			backgroundColor: 'gray',
+			healthColor: 'red',
+			leftTopCorner: {
+				x: 10,
+				y: 55
+			},
+			width: 154,
+			height: 14
+		},
+		manaBarConfig: {
+			borderColor: 'black',
+			borderWidth: 2,
+			backgroundColor: 'gray',
+			manaColor: 'blue',
+			leftTopCorner: {
+				x: 10,
+				y: 80
+			},
+			width: 154,
+			height: 14
+		}
 	}
 })()
