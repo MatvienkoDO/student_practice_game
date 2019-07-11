5ee63146ddbb17c8c2997e6e4b0d7a9211c59da4
Cobuch <52578597+Cobuch@users.noreply.github.com>
2019-07-11 17:15:25 +0400

    Update input-initialization.js
    Controllers

--- scripts/input-initialization.js
+++ scripts/input-initialization.js
@@ -1,2 +1,22 @@
 (function(){
+  function playerMovement(e){
+    switch(e.keyCode){
+        case 37:
+            if(state.playerPos.x > 0){
+                state.playerPos.x -= state.playerSpeed;
+                state.characterAnimation = 'run';
+                break;}
+            else if(state.playerPos.x == 0) break;
+        case 39: 
+           if(state.playerPos.x < 1180){ 
+                state.playerPos.x += state.playerSpeed;
+                state.characterAnimation = 'run';
+            break;
+        }
+    }
+    $(document).keyup(function (e){ 
+    state.characterAnimation = 'idle'; 
+    }) 
+}
+addEventListener("keydown", playerMovement);
 })()
