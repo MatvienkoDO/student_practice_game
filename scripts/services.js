fe044f8d40999f1e2dd2531d73aff3a87faaec67
Cobuch <52578597+Cobuch@users.noreply.github.com>
2019-07-14 00:34:48 +0400

    Update services.js

--- scripts/services.js
+++ scripts/services.js
@@ -1,6 +1,7 @@
 (function(){
 	setInterval(stateChangers.changePlayerFrame, 50)
 	setInterval(stateChangers.changeEnemyFrame, 60)
+	setInterval(stateChangers.changeBulletFrame, 120)
 
 	const movementInterval = 10
 	const movementCallback = () => {
