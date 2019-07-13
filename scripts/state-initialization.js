f9148674a71259bac01edc4e0b23d5de39f38637
Cobuch <52578597+Cobuch@users.noreply.github.com>
2019-07-14 00:30:56 +0400

    Update state-initialization.js

--- scripts/state-initialization.js
+++ scripts/state-initialization.js
@@ -5,12 +5,15 @@ const state = (function(){
 		health: 100,
 		mana: 100,
 		startTime: null,
-		playerPos: { x: 10, y: 390 },
-		playerSpeed: 0,
+		playerPos: { x: 0, y: 0 },
+		playerTurnedTo: 'right',
 		characterFrameNumber: 0,
 		characterAnimation: 'idle',
 		enemyPos:{ x: 1000, y: 390 },
 		skeletonFrameNumber: 0,
-		skeletonAnimation: 'walk'
+		skeletonAnimation: 'walk',
+		bulletPos:{ x: 0, y: 0 },
+		bulletFrameNumber: 0,
+		bulletAnimation: 'water',
 	}
 })()
