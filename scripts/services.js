77851f5754a7bc11a6a0f7a95d18c9f8137ab0d8
Cobuch <52578597+Cobuch@users.noreply.github.com>
2019-07-15 20:00:17 +0400

    Update services.js

--- scripts/services.js
+++ scripts/services.js
@@ -1,10 +1,15 @@
 (function(){
 	setInterval(stateChangers.changePlayerFrame, 50)
-	setInterval(stateChangers.changeEnemyFrame, 60)
+	setInterval(stateChangers.changeEnemyFrame, 55)
 
 	const movementInterval = 10
 	const movementCallback = () => {
-		stateChangers.playerMovement(movementInterval / 1000)
+		stateChangers.playerMovement(movementInterval / 1000),
+		stateChangers.skeletonMovement()
+		
 	}
+	setInterval(stateChangers.skeletonDamageDealing, 1000)
 	setInterval(movementCallback, movementInterval)
+	setInterval(stateChangers.healthRegeneration, 1000)
+	setInterval(stateChangers.manaRegeneration, 1000)
 })()
